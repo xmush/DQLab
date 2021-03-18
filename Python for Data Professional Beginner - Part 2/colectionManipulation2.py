da60dfa35fd2b170ce36023dc2b4632f2dce4fe9
xmush <xmushdev@gmail.com>
2021-03-18 09:49:36 +0700

    first commit

---
+++ Python for Data Professional Beginner - Part 2/colectionManipulation2.py
@@ -0,0 +1,7 @@
+list_makanan = ['Gado-gado', 'Ayam Goreng', 'Rendang']
+list_minuman = ['Es Teh', 'Es Jeruk', 'Es Campur']
+list_menu = list_makanan + list_minuman
+print(list_menu)
+
+# harga = [1000, 2500, 5000, 15000, 30000]
+# print(harga[:-3])
